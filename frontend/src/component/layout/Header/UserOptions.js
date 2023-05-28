36a1d93638195614b27bd5c8866cd664d4c2c7f3
yujit2003 <yujityadav99@gmail.com>
2023-05-29 05:08:21 +0530

    first commit

--- frontend/src/component/layout/Header/UserOptions.js
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -37,13 +37,13 @@ const UserOptions = ({ user }) => {
     { icon: <ExitToAppIcon />, name: "Logout", func: logoutUser },
   ];
 
-  if (user.role === "admin") {
-    options.unshift({
-      icon: <DashboardIcon />,
-      name: "Dashboard",
-      func: dashboard,
-    });
-  }
+  // if (user.role === "admin") {
+  //   options.unshift({
+  //     icon: <DashboardIcon />,
+  //     name: "Dashboard",
+  //     func: dashboard,
+  //   });
+  // }
 
   function dashboard() {
     // navigator("/admin/dashboard");
